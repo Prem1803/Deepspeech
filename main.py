9be44520abf72972b17abce4d41c292e161281ee
Prem1803 <prem47645@gmail.com>
2023-04-07 18:46:27 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import deepspeech
+import numpy as np
+import wave
+
+# create a DeepSpeech model with the downloaded pre-trained models
+model = deepspeech.Model('deepspeech-0.9.3-models.pbmm')
+model.enableExternalScorer('deepspeech-0.9.3-models.scorer')
+
+# load an audio file to transcribe
+with wave.open('audio/2830-3980-0043.wav', 'rb') as w:
+    rate = w.getframerate()
+    frames = w.getnframes()
+    buffer = w.readframes(frames)
+
+# transcribe the audio file
+text = model.stt(np.frombuffer(buffer,dtype=np.int16))
+print(text)
