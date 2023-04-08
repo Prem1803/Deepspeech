0adcf5e478fafd43d6f0ad3e7f73b4fd6364ee76
Prem Kumar <Prem.Kumar@Fareportal.Com>
2023-04-08 11:30:36 +0530

    Added vad trabscriber

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 import wave
 
 # create a DeepSpeech model with the downloaded pre-trained models
-model = deepspeech.Model('deepspeech-0.9.3-models.pbmm')
-model.enableExternalScorer('deepspeech-0.9.3-models.scorer')
+model = deepspeech.Model('./deepseech-model/deepspeech-0.9.3-models.pbmm')
+model.enableExternalScorer('./deepseech-model/deepspeech-0.9.3-models.scorer')
 
 # load an audio file to transcribe
 with wave.open('audio/2830-3980-0043.wav', 'rb') as w:
@@ -14,4 +14,4 @@ with wave.open('audio/2830-3980-0043.wav', 'rb') as w:
 
 # transcribe the audio file
 text = model.stt(np.frombuffer(buffer,dtype=np.int16))
-print(text)
+print(f"\nTranscription : {text}")
