e469225612552ef6d90a8fa5e32f2a2387ced602
Prem Kumar <Prem.Kumar@Fareportal.Com>
2023-04-08 11:37:00 +0530

    Updated Model Path

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 import wave
 
 # create a DeepSpeech model with the downloaded pre-trained models
-model = deepspeech.Model('./deepseech-model/deepspeech-0.9.3-models.pbmm')
-model.enableExternalScorer('./deepseech-model/deepspeech-0.9.3-models.scorer')
+model = deepspeech.Model('deepspeech-model/deepspeech-0.9.3-models.pbmm')
+model.enableExternalScorer('deepspeech-model/deepspeech-0.9.3-models.scorer')
 
 # load an audio file to transcribe
 with wave.open('audio/2830-3980-0043.wav', 'rb') as w:
